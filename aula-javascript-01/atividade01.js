712fee4d62a95314911ec208a4408df61253d387
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-08-23 18:49:21 -0300

    feat: atividades modulo js aula 01

---
+++ aula-javascript-01/atividade01.js
@@ -0,0 +1,15 @@
+// 1) Criar variáveis com tipos de dados:
+// a) Number
+let numero = 0;
+// b) String
+let nome = "";
+// c) Boolean
+let vivo = true;
+// d) Array
+let infos = []
+// e) Object
+let atividades = {}
+// f) Undefined
+let casamento;
+// g) Null
+let animaDeEstimacao = null;
---
+++ aula-javascript-01/atividade02.js
@@ -0,0 +1,9 @@
+// 1)      Criar duas variáveis usando a palavra-chave var.
+var nome = "Danilo";
+var vivo = true;
+// 2)      Criar duas variáveis usando a palavra-chave let.
+let sexo = "Masculino";
+let idade = 20
+// 3)      Criar duas constantes usando a palavra-chave const.
+const dtNascimento = "08/10/2001";
+const genitora = "Daniela";
