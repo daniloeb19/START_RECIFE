26e970853e0eb78e8ee8f32bbd94929ae788a65a
Danilo Ferreira <daniloferreiradasilva345@gmail.com>
2022-08-24 13:58:29 -0300

    feat: atividades start repositorio 6

---
+++ aula-javascript-06/atividade1.js
@@ -0,0 +1,25 @@
+// Criar um arquivo javascript com as atividade 1 e 2
+// 1)      Criar 3 variáveis:
+// a)       A primeira variável deverá conter o seu nome. 
+// b)      A segunda variável deverá conter a sua idade.
+// c)       Converter a segunda variável para string.
+// d)      A terceira variável deverá conter a concatenação das duas variáveis anteriores.
+// e)      Exibir no console a terceira variável.
+// f)        Exibir no console o tamanho da terceira variável.
+let nome = "Danilo";
+let idade = 20;
+idade = idade.toString();
+let concatenacao = nome +" "+idade;
+console.log(concatenacao);
+console.log(concatenacao.length);
+
+// 2)      Criar 2 variáveis:
+// a)       A primeira variável deverá conter o valor 17.
+// b)      A segunda variável deverá conter o valor 28.
+// c)       Converter os valores das duas variáveis
+// d)       Exibir no console a terceira variável.
+
+let val1 = 17;
+let val2 = 28;
+let val3 = val1.toString()+val2.toString();
+console.log(val3);
---
+++ aula-javascript-06/atividade2.js
@@ -0,0 +1,15 @@
+let txt = "abcdefghijklm";
+console.log(txt.indexOf("h"))
+txt = "Posso comer bananas o dia todo";
+console.log(txt.split(" ").slice(2, 3).toString());
+txt = "Olá, mundo";
+console.log(txt.replace("Olá", "Bem vindo"));
+console.log(txt.toUpperCase());
+console.log(txt.toLowerCase());
+txt = "Posso comer bananas o dia todo";
+console.log(txt.length);
+let str1 = "Hello ";
+let str2 = "World!";
+console.log(str1 + str2);
+
+//exercicios repetidos
